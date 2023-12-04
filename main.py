0487e6a50605c15ace834bef7badd6736274b8db
George <Linkeydoo@gmail.com>
2023-12-03 20:24:19 -0500

    Commented Code

--- main.py
+++ main.py
@@ -18,79 +18,104 @@ def asci_to_binary(secret_message):
         return binary_data
  
 # Pixels are modified according to the 8-bit binary data and finally returned
-def modPix(picture, secret_message):
+def modPix(group_three_pixels, secret_message):
     binary_data = asci_to_binary(secret_message)
     data_length = len(binary_data)
-    data_from_image = iter(picture)
+    data_from_image = iter(group_three_pixels)
+
+    # Counts how many chunks of 3 pixels we ran through
     i = 1
+
+    # counts how many non transparent pixels we stored info to
     loop_counter = 0
+    # Counts how many pixels we passed. Used for calculating where to put the pixels
     pixel_number = 0
     # SO the issue is that we run through the image datalength before hitting a non transparent pixel
     while loop_counter < data_length:
         # Extracting 3 pixels at a time
         # data_from_image.__next__()[:3] pulls the last 3 bytes from that color channel the pixel Ex: (254, 255, 254)
-        picture = [value for value in data_from_image.__next__()[:3] +
+        group_three_pixels = [value for value in data_from_image.__next__()[:3] +
                                 data_from_image.__next__()[:3] +
                                 data_from_image.__next__()[:3]]
+        # we grabbed 3 pixels so now we need to add 3 to the total count
         pixel_number += 3
-        # Pixel value should be made odd for 1 and even for 0
+
+        # Pixels are assumed to not be transparent
         has_transparency = False
-        for i in picture:
-            if i > 250:
+        for pixel_channel_value in group_three_pixels:
+            #Then we check if the value is larger then 254
+            if pixel_channel_value > 250:
                 has_transparency=True
         if not has_transparency:
-            print("Data " + str(i) + " Pre Encoded: " + str(picture))
+            print("Data " + str(i) + " Pre Encoded: " + str(group_three_pixels))
+            
+            # Pixel value should be made odd for 1 and even for 0
             for j in range(0, 8):
-                if (binary_data[loop_counter][j] == '0' and picture[j]% 2 != 0):
-                    picture[j] -= 1
+                if (binary_data[loop_counter][j] == '0' and group_three_pixels[j]% 2 != 0):
+                    group_three_pixels[j] -= 1
     
-                elif (binary_data[loop_counter][j] == '1' and picture[j] % 2 == 0):
-                    if(picture[j] != 0):
-                        picture[j] -= 1
+                elif (binary_data[loop_counter][j] == '1' and group_three_pixels[j] % 2 == 0):
+                    if(group_three_pixels[j] != 0):
+                        group_three_pixels[j] -= 1
                     else:
-                        picture[j] += 1
+                        group_three_pixels[j] += 1
                     # pix[j] -= 1
     
             # Eighth pixel of every set tells whether to stop ot read further. 0 means keep reading; 1 means the message is over.
             if (i == data_length - 1):
-                if (picture[-1] % 2 == 0):
-                    if(picture[-1] != 0):
-                        picture[-1] -= 1
+                if (group_three_pixels[-1] % 2 == 0):
+                    if(group_three_pixels[-1] != 0):
+                        group_three_pixels[-1] -= 1
                     else:
-                        picture[-1] += 1
+                        group_three_pixels[-1] += 1
     
             else:
-                if (picture[-1] % 2 != 0):
-                    picture[-1] -= 1
+                if (group_three_pixels[-1] % 2 != 0):
+                    group_three_pixels[-1] -= 1
     
-            picture = tuple(picture)
-            print("Data " + str(i) + " Encoded: " + str(picture))
-            yield (picture[0:3], pixel_number)
-            yield (picture[3:6], pixel_number)
-            yield (picture[6:9], pixel_number)
+            group_three_pixels = tuple(group_three_pixels)
+            print("Data " + str(i) + " Encoded: " + str(group_three_pixels))
+            # yeald is like return for generators.
+            # remember we increment pixel_number by 3, so we need to decrement it here to get the right pixel number
+            yield (group_three_pixels[0:3], pixel_number-2)
+            yield (group_three_pixels[3:6], pixel_number-1)
+            yield (group_three_pixels[6:9], pixel_number)
+
+            # only increment the loop counter when we hit a non transparent pixel.
             loop_counter = loop_counter + 1
+        
         i = i + 1 
  
 def encode_enc(newimg, data):
     image_width = newimg.size[0]
     (x, y) = (0, 0)
     # data_from_image = iter(newimg.getdata())
-    pixel_counter = 0
+
+    # Counts where we are in the current image (i dont think we need this because of the new param)
+    # pixel_counter = 0
     for pixel in modPix(newimg.getdata(), data):
-        x = pixel[1] + pixel_counter
+        print()
+        # Sets the current pixel to the 
+        # x = pixel[1] + pixel_counter
         print("pixel Number:", pixel[1])
+
+        # Sets X to the pixel Number
+        x = pixel[1]
         print("image width:", image_width)
         if (x > image_width - 1):
             y = math.floor(x/image_width)
             x = x % (image_width)
             print(y)
         else:
-            x = pixel[1] + pixel_counter
-        # newimg.putpixel((x, y), pixel[0])
-        newimg.putpixel((x, y), (255, 0, 0))
-        pixel_counter += 1
-        if pixel_counter == 3:
-            pixel_counter = 0
+            # x = pixel[1] + pixel_counter
+            x = pixel[1]
+        # Uncomment the below like for writing real data    
+        newimg.putpixel((x, y), pixel[0])
+        # uncomment the below line to yellow test pixels to see where data is being written
+        # newimg.putpixel((x, y), (245, 245, 0))
+        # pixel_counter += 1
+        # if pixel_counter == 3:
+        #     pixel_counter = 0
         
         
  
@@ -122,16 +147,10 @@ def decode():
     plain_text = ''
     imgdata = iter(stego_file.getdata())
     
-    # skip_three = False
     while (True):
         pixels = [value for value in imgdata.__next__()[:3] +
                                 imgdata.__next__()[:3] +
                                 imgdata.__next__()[:3]]
-        if skip_three:
-            skip_three = False
-            continue
-        skip_three = True
-        # string of binary data
         binstr = ''
         has_transparency = False
         for i in pixels:
@@ -149,31 +168,6 @@ def decode():
                 if (pixels[-1] % 2 != 0):
                     return plain_text[:-2]
 
-# def decode():
-#     stego_file_name = input("Enter image name(with extension) : ")
-#     stego_file = Image.open(stego_file_name, 'r')
- 
-#     plain_text = ''
-#     imgdata = iter(stego_file.getdata())
- 
-#     while (True):
-#         pixels = [value for value in imgdata.__next__()[:3] +
-#                                 imgdata.__next__()[:3] +
-#                                 imgdata.__next__()[:3]]
-        
-#         # string of binary data
-#         binstr = ''
-#         # Odd = 1, even = 0.
-#         for i in pixels[:8]:
-#             if (i % 2 == 0):
-#                 binstr += '0'
-#             else:
-#                 binstr += '1'
- 
-#         plain_text += chr(int(binstr, 2))
-#         if (pixels[-1] % 2 != 0):
-#             return plain_text
-        
 
 # Main Function
 def main():
